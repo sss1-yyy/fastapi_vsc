2312184e9df9373c80c2b8321f3aea783d3726ec
sss1-yyy <ksui9210@gmail.com>
2025-07-15 15:33:13 +0900

    요청 예제 데이터 선언. 및 JSON 본문 선언

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-from typing import Set, Union
+from typing import Annotated
 
-from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
 class Item(BaseModel):
     name: str
-    description: Union[str, None] = None
+    description: str | None = None
     price: float
-    tax: Union[float, None] = None
-    tags: Set[str] = set()
-    image: Union[Image, None] = None
+    tax: float | None = None
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
     results = {"item_id": item_id, "item": item}
     return results
