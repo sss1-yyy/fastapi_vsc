af0407e7585e267a93e853bc558d069df9b752ef
sss1-yyy <ksui9210@gmail.com>
2025-07-17 23:58:29 +0900

    Pydantic 모델을 사용한 헤더
    코드에서 쿠키와 헤더랑 다른 건, class 이름이랑, 이름 정의할 때, Annotated 2번째 인자가 Cookie()라는 것 밖에 없음.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from typing import Union
+from typing import Annotated
 
 from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
-app = FastAPI()
+app=FastAPI()
 
+class CommonHeaders(BaseModel):
+  host : str
+  save_data : bool
+  if_modified_since : str | None = None
+  traceparent : str | None = None
+  x_tag : list[str] = []
 
 @app.get("/items/")
-async def read_items(
-    strange_header: Union[str, None] = Header(default=None, convert_underscores=False),
-):
-    return {"strange_header": strange_header}
+async def read_items(headers: Annotated[CommonHeaders, Header()]):
+  return headers
