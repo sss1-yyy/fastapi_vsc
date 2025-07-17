d0a759bfe515913e013157ca72d74e56076e59ea
sss1-yyy <ksui9210@gmail.com>
2025-07-18 02:48:02 +0900

    응답 모델 (응답 나타내는 방법들)

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-from typing import Annotated
+from typing import Union
 
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
 from pydantic import BaseModel
 
-app=FastAPI()
+app = FastAPI()
 
-class CommonHeaders(BaseModel):
-  host : str
-  save_data : bool
-  if_modified_since : str | None = None
-  traceparent : str | None = None
-  x_tag : list[str] = []
 
-@app.get("/items/")
-async def read_items(headers: Annotated[CommonHeaders, Header()]):
-  return headers
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: float = 10.5
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz",
+        "description": "There goes my baz",
+        "price": 50.2,
+        "tax": 10.5,
+    },
+}
+
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+    response_model_include=["name", "description"],
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude=["tax"])
+async def read_item_public_data(item_id: str):
+    return items[item_id]
