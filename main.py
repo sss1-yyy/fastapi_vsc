7b1b2db07a0f467e9fd7d9ecb908371a1381e9eb
sss1-yyy <ksui9210@gmail.com>
2025-07-19 14:46:22 +0900

    폼 모델

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from typing import Annotated
 
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 
-@app.post("/items/", status_code=201)
-async def create_item(name: str):
-    return {"name": name}
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
