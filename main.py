6691a28d4e3720841ef8633f2e05e50eb1199692
sss1-yyy <ksui9210@gmail.com>
2025-07-27 22:54:26 +0900

    메타데이터 및 문서화 URL (기본값 수정하기)

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from typing import Union
+from fastapi import FastAPI
 
-from fastapi import BackgroundTasks, Depends, FastAPI
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
 
-app = FastAPI()
+app = FastAPI(openapi_tags=tags_metadata)
 
 
-def write_log(message: str):
-    with open("log.txt", mode="a") as log:
-        log.write(message)
+@app.get("/users/", tags=["users"])
+async def get_users():
+    return [{"name": "Harry"}, {"name": "Ron"}]
 
 
-def get_query(background_tasks: BackgroundTasks, q: Union[str, None] = None):
-    if q:
-        message = f"found query: {q}\n"
-        background_tasks.add_task(write_log, message)
-    return q
-
-
-@app.post("/send-notification/{email}")
-async def send_notification(
-    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
-):
-    message = f"message to {email}\n"
-    background_tasks.add_task(write_log, message)
-    return {"message": "Message sent"}
+@app.get("/items/", tags=["items"])
+async def get_items():
+    return [{"name": "wand"}, {"name": "flying broom"}]
