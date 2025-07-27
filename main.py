17a07a22b0616ede37ffc1c2655a666ebe1f2b1f
sss1-yyy <ksui9210@gmail.com>
2025-07-27 22:36:25 +0900

    백그라운드 작업 main.py에 하기

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from fastapi import Depends
-from fastapi import FastAPI
-from .dependencies import get_query_token, get_token_header
-from .internal import admin
-from .routers import items, users
+from typing import Union
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+from fastapi import BackgroundTasks, Depends, FastAPI
+
+app = FastAPI()
 
 
-app.include_router(users.router)
-app.include_router(items.router)
-app.include_router(
-    admin.router,
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(get_token_header)],
-    responses={418: {"description": "I'm a teapot"}},
-)
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+def get_query(background_tasks: BackgroundTasks, q: Union[str, None] = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
+
+
+@app.post("/send-notification/{email}")
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent"}
